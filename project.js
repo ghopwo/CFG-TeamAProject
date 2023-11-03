f422f9194fdf1a176377996db59359ed2c33ab2d
ghopwo <144729912+ghopwo@users.noreply.github.com>
2023-11-03 18:11:55 +0000

    Add files via upload
    correct version

---
+++ project.js
@@ -0,0 +1,16 @@
+$(window).resize(function() {
+    var screenWidth = $(window).width();
+  
+    if (screenWidth <= 575.98) {
+      $(".accordion").width("100%");
+      $(".accordion-item").css("margin-bottom", "0");
+      $(".accordion-button").width("100%");
+    } else {
+      $(".accordion").width("auto");
+      $(".accordion-item").css("margin-bottom", "");
+      $(".accordion-button").width("");
+    }
+  });
+  const carousel = new bootstrap.Carousel('#myCarousel')
+
+ 
